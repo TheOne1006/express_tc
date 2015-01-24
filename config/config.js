dfbfb1e14f5519e69a9cec55735a1d8e71789ea8
theone <297190869@qq.com>
2015-01-24 10:02:39 +0800

    init

---
+++ config/config.js
@@ -0,0 +1,37 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'express-tc'
+    },
+    port: 3000,
+    /**
+     * 监听12345端口
+     */
+    db: 'mongodb://127.0.0.1:12345'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'express-tc'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/express-tc-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'express-tc'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/express-tc-production'
+  }
+};
+
+module.exports = config[env];
