e5838737d157aee05e844806fddc38dc54785ca8
theone <theone@theone-ThinkPad-E430c.(none)>
2015-02-27 13:48:27 +0800

    通过modal添加照片，未完成上传

--- angular/scripts/controllers/admin/adminUserCtrl.js
+++ angular/scripts/controllers/admin/adminUserCtrl.js
@@ -49,14 +49,52 @@ angular.module('theoneApp')
       };
     }])
     // 添加管理员头像
-    .controller('UserPhotoAddController', ['$scope','$modalInstance', function ($scope, $modalInstance) {
-      
-      $scope.ok = function () {
-        $modalInstance.close();
+    .controller('UserPhotoAddController', ['$scope','$modalInstance','$window', function ($scope, $modalInstance, $window) {
+      // 监控 windows.Webcam
+      var webcam = $window.Webcam;
+      // 照片列表
+      $scope.photoList = [];
+
+      if(webcam){
+          webcam.set({
+            width: 360,
+            height: 280,
+            'image_format': 'jpeg',
+            'jpeg_quality': 90
+          });
+      }
+
+      $scope.begin = function () {
+          webcam.attach('#modalMovie');
       };
 
       $scope.cancel = function () {
+        if(webcam.container){
+          webcam.reset();
+        }
         $modalInstance.dismiss();
       };
+
+      $scope.delphoto = function  (key) {
+        $scope.photoList.splice(key,1);
+      };
+      
+      $scope.ok = function () {
+        if(webcam.container){
+          webcam.snap(function (data) {
+            $scope.photoList.push(data);
+          });
+        }
+      };
+
+      $scope.upload = function () {
+        if(!$scope.photoList){
+          return;
+        }
+        
+
+      };
+
+
       
     }]);
