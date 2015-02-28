07e2e48b0ddf6af74a06fff45532bf3e256fb616
theone <theone@theone-ThinkPad-E430c.(none)>
2015-02-28 18:02:04 +0800

    ready adminph

---
+++ app/models/adminph.js
@@ -0,0 +1,14 @@
+/**
+ * 管理员
+ * 身份照片
+ */
+'use strict';
+
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  moment = require('moment');
+
+  //定义图片模型
+  var AdminphSchema = new Schema({
+    
+  });
