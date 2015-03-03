722291a21bf963b5f6ddba0c29cb5f41d160d59f
theone <theone@theone-ThinkPad-E430c.(none)>
2015-03-03 12:01:39 +0800

    login 自动照相完成

--- angular/scripts/controllers/admin/login.js
+++ angular/scripts/controllers/admin/login.js
@@ -1,6 +1,6 @@
 'use strict';
 /**
- * login 路由
+ * login router
  */
  angular.module('theoneApp')
    .config(function($stateProvider,$urlRouterProvider) {
@@ -24,6 +24,61 @@
  *
  */
 angular.module('theoneApp')
+  .factory('webcamService', ['$http', '$window', '$interval', function($http, $window, $interval){
+    var webcam = $window.Webcam,
+    phsListArr = [],
+    takePhotoInterval,
+    webcamParams = {
+      width: 360,
+      height: 280,
+      'dest_width': 180,
+      'dest_height': 140,
+      'image_format': 'jpeg',
+      'jpeg_quality': 90
+    };
+
+    var setWebcam = function  (options) {
+      angular.extend(webcamParams, options);
+      webcam.set(webcamParams);
+    };
+
+    var startWebcam = function  (domId) {
+      webcam.attach(domId);
+      webcam.on('live',function  () {
+        intervalGetPhoto();
+      });
+    };
+
+    var intervalGetPhoto = function  () {
+      takePhotoInterval = $interval(function  () {
+        if(webcam.container){
+          webcam.snap(function (data) {
+            phsListArr.push(data);
+            console.log(data);
+          });
+        }else{
+          takePhotoInterval.cancel();
+        }
+      },5000);
+    };
+
+
+
+    return {
+      readyWebcam:function (options, domId) {
+        setWebcam(options);
+        startWebcam(domId);
+      },
+      takePhotos : function () {
+        if(!webcam.container){
+          return;
+        }
+      },
+      getWebcam:function  () {
+        return webcam.container;
+      }
+    };
+  }])
   .controller('LoginController',['$scope', '$window', '$http',function ($scope, $window, $http) {
   $scope.user = {
     name:'',
@@ -34,6 +89,17 @@ angular.module('theoneApp')
     $http.post('/admin/verify/password',{user:$scope.user}).success(function  (data) {
       console.log(data);
     });
-
   };
-}]);
+}])
+  .controller('LoginWebcamController',['$scope','$timeout','$window', 'webcamService', function($scope, $timeout, $window, webcamService){
+
+    $scope.phListArr = [];
+
+    $timeout(function () {
+      webcamService.readyWebcam(null, '#photo');
+    },1000);
+
+
+
+  }])
+  ;
