a2696eace1bcdfe04727e1f16fa0444b98bbcfd9
theone <297190869@qq.com>
2015-03-05 01:46:26 +0800

    -ready check

--- app/controllers/admin/login.js
+++ app/controllers/admin/login.js
@@ -5,6 +5,8 @@
 var express = require('express'),
   router = express.Router(),
   mongoose = require('mongoose'),
+  config = require('../../../config/config'),
+  help =require(config.root+'/my_node_modules/theone-help'),
   crypto = require('crypto'),
   async = require('async'),
   moment = require('moment'),
@@ -24,8 +26,22 @@ var express = require('express'),
         res.end('ok');
     })
     .post('/verify/face', function (req, res, next) {
-        console.log(req.files);
-        res.end();
+        // var img = req.files.webcam;
+        async.waterfall([
+          function (cb) {
+            help.facePlusPlusDetect('http://res.cloudinary.com/theone/image/upload/v1425302230/mavjc53omfyfnngyjebw.jpg', cb);
+          },
+          function (detectInfo, cb) {
+            if(!detectInfo || !detectInfo.face || !detectInfo.face[0] ||!detectInfo.face[0].face_id){
+              return cb({err:'no face_id'});
+            }
+          }
+          ],function (err) {
+          if(err){
+            return next(err);
+          }  
+          res.end();
+          });
     })
 
 ;
